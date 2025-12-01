aa6d1c106a06c1f2985221d6d0c1707c9f143e80
Anup Kumar <your_github_rachitjha999@gmail.com>
2025-12-01 20:51:02 +0530

    fick

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -26,7 +26,7 @@ export default function Contact() {
     setSubmitted(true);
 
     
-const res = await fetch(`https://nexepwebsite.onrender.com//send-email`, {
+const res = await fetch(`https://nexepwebsite.onrender.com/send-email`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
