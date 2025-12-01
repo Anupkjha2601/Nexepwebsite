b064e2c88da62b8681aaa4e5bfe645f6a8a2b2b8
Anup Kumar <your_github_rachitjha999@gmail.com>
2025-12-01 11:42:09 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://nexepwebsite.onrender.com",
+  methods: ["GET","POST","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
+}));
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
